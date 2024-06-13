23116a83f746fd2891cb1679f9ff714177b04c04
Frederik <frederik.kunstner@gmail.com>
2024-06-13 13:05:26 -0700

    remove unused problems

--- src/optexp/problems/__init__.py
+++ src/optexp/problems/__init__.py
@@ -1,11 +1,3 @@
+from optexp.problems.classification import Classification
 from optexp.problems.problem import Problem
-from optexp.problems.classification import (
-    Classification,
-    FullBatchClassification,
-    SquaredLossClassification,
-    FullBatchSquaredLossClassification,
-    ClassificationWithPerClassStats,
-)
-from optexp.problems.regression import Regression, FullBatchRegression
-from optexp.problems.transformer import Transformer, FullBatchTransformer
 from optexp.problems.utils import DivergingException
