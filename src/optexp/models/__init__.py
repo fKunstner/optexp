4168dc8b2329c40466dbfea4c367e5f0d70e327c
Frederik <frederik.kunstner@gmail.com>
2024-06-13 16:11:55 -0700

    Fix import error

--- src/optexp/models/__init__.py
+++ src/optexp/models/__init__.py
@@ -1,3 +1,4 @@
-from vision import LeNet5, ResNet
+from optexp.models.model import Model
+from optexp.models.vision import LeNet5, ResNet
 
-__all__ = ["LeNet5", "ResNet"]
+__all__ = ["Model", "LeNet5", "ResNet"]
