aea91fbd8ba260aca0d5a17824d8f959eaab05ff
Frederik <frederik.kunstner@gmail.com>
2024-08-06 12:02:50 -0700

    export pipes in api

--- optexp/__init__.py
+++ optexp/__init__.py
@@ -1,4 +1,4 @@
-from optexp import datasets, hardwareconfig, metrics, models, optim
+from optexp import datasets, hardwareconfig, metrics, models, optim, pipes
 from optexp.cli import cli
 from optexp.config import use_wandb_config
 from optexp.experiment import Experiment
