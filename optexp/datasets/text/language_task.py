63643e14802c26feeb77f5ad7d7522dd5176b63d
Frederik <frederik.kunstner@gmail.com>
2024-12-22 00:30:48 +0100

    Finish PredictMiddleToken task and add tests

--- optexp/datasets/text/language_task.py
+++ optexp/datasets/text/language_task.py
@@ -1,5 +1,6 @@
 import torch
 from attr import frozen
+from numpy.lib.stride_tricks import as_strided, sliding_window_view
 
 from optexp.component import Component
 
@@ -54,10 +55,20 @@ class PredictNextToken(LanguageTask):
 
 @frozen
 class PredictMiddleToken(LanguageTask):
+    allow_overlap: bool = False
     """
+    
+    args:
+        allow_overlap: If True, the sequences can overlap.
+            If True, overlapping sequences will be generated, e.g. tokens [1,2,3,4] and sequence 
+            length 3 will generate [1,2,3] and [2,3,4]. If False, only non-overlapping sequences,
+            e.g. [1,2,3].
+    
     The format of the data matrices is as follows:
     sequences_mat: (n_sequences, sequence_len)
     targets_mat: (n_sequences)
+    
+    all_sequences: If True, the targets are all the tokens in the sequence. Eg;
 
     For each sequence, the targets is the token in the middle of the sequence
 
@@ -71,19 +82,39 @@ class PredictMiddleToken(LanguageTask):
             1,
             4,
         ]
+        
+    If allow_overlap is True, all sequences (including overlap) are generated, e.g.:
+    
+        Tokens: [0,1,2,3,4,5]
+        
+        sequences_mat = [
+            [0,1,2],
+            [1,2,3],
+            [2,3,4],
+            [3,4,5],
+        ]
+        targets_mat = [
+            1,
+            2,
+            3,
+            4,
+        ]
+    
     """
+
+    def generate_subsequences(
+        self, tokens: torch.Tensor, sequence_len: int
+    ) -> torch.Tensor:
+        if self.allow_overlap:
+            return tokens.unfold(0, sequence_len, 1)
+        else:
+            return tokens.unfold(0, sequence_len, sequence_len)
 
     def tokens_to_sequences_and_targets(
         self, tokens: torch.Tensor, sequence_len: int
     ) -> tuple[torch.Tensor, torch.Tensor]:
-        n_tokens = tokens.size()[0]
-        n_sequences = n_tokens // sequence_len
+        sequences_mat = self.generate_subsequences(tokens, sequence_len)
+        index_of_middle_token = sequence_len // 2
+        targets_mat = sequences_mat[:, index_of_middle_token]
 
-        last_target_is_incomplete = n_tokens < (n_sequences * sequence_len) + 1
-        if last_target_is_incomplete:
-            n_sequences -= 1
-
-        sequences_mat = tokens[0 : n_sequences * sequence_len]
-        sequences_mat = sequences_mat.view(n_sequences, sequence_len)
-
-        raise NotImplementedError
+        return sequences_mat, targets_mat
--- tests/test_sequencer.py
+++ tests/test_sequencer.py
@@ -1,42 +1,78 @@
+from functools import partial
 from typing import List
 
 import pytest
 import torch
 
-from optexp.datasets.text.wikitext import tokens_to_sequences_and_targets
+from optexp.datasets.text.language_task import PredictMiddleToken, PredictNextToken
 
 
 @pytest.mark.parametrize(
-    "tokens,sequence_len,true_sequences,true_targets",
+    "task,tokens,sequence_len,true_sequences,true_targets",
     [
         (
+            PredictNextToken,
             torch.tensor([1, 2, 3]),
             2,
             [torch.tensor([1, 2])],
             [torch.tensor([2, 3])],
         ),
         (
+            PredictNextToken,
             torch.tensor([1, 2, 3, 4]),
             2,
             [torch.tensor([1, 2])],
             [torch.tensor([2, 3])],
         ),
         (
+            PredictNextToken,
             torch.tensor([1, 2, 3, 4, 5]),
             2,
             [torch.tensor([1, 2]), torch.tensor([3, 4])],
             [torch.tensor([2, 3]), torch.tensor([4, 5])],
         ),
+        (
+            PredictMiddleToken,
+            torch.tensor([1, 2, 3, 4, 5, 6]),
+            3,
+            torch.tensor(
+                [
+                    [1, 2, 3],
+                    [4, 5, 6],
+                ]
+            ),
+            torch.tensor([2, 5]),
+        ),
+        (
+            partial(PredictMiddleToken, allow_overlap=True),
+            torch.tensor([1, 2, 3, 4, 5, 6]),
+            3,
+            torch.tensor(
+                [
+                    [1, 2, 3],
+                    [2, 3, 4],
+                    [3, 4, 5],
+                    [4, 5, 6],
+                ]
+            ),
+            torch.tensor([2, 3, 4, 5]),
+        ),
     ],
 )
-def test_sequence(tokens, sequence_len, true_sequences, true_targets):
-    sequences_list, targets_list = tokens_to_sequences_and_targets(tokens, sequence_len)
+def test_sequence(task, tokens, sequence_len, true_sequences, true_targets):
+    sequences_list, targets_list = task().tokens_to_sequences_and_targets(
+        tokens, sequence_len
+    )
     assert all([torch.equal(a, b) for a, b in zip(sequences_list, true_sequences)])
     assert all([torch.equal(a, b) for a, b in zip(targets_list, true_targets)])
 
 
 if __name__ == "__main__":
+
     test_sequence(
-        tokens=torch.tensor([1, 2]),
-        sequence_len=2,
+        PredictMiddleToken,
+        torch.tensor([1, 2, 3, 4, 5, 6]),
+        3,
+        [torch.tensor([1, 2, 3]), torch.tensor([2])],
+        [torch.tensor([4, 5, 6]), torch.tensor([5])],
     )
