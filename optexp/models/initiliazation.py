2638f9d9e0163f577c6f63d11652ae5bd4817ebe
Frederik <frederik.kunstner@gmail.com>
2025-01-21 14:16:56 +0100

    Make initialization strategy ignore encode if note present

--- optexp/models/initiliazation.py
+++ optexp/models/initiliazation.py
@@ -2,11 +2,16 @@ import math
 from abc import ABC, abstractmethod
 
 import torch.nn
+from attr import frozen
 
 from optexp.component import Component
 
 
-class InitializationStrategy(ABC, Component):
+@frozen
+class InitializationStrategy(
+    Component,
+    ABC,
+):
     """Abstract base class for parameter initialization"""
 
     @abstractmethod
@@ -20,13 +25,11 @@ class DefaultInitialization(InitializationStrategy):
 
 
 # mostly stolen from https://github.com/karpathy/nanoGPT/blob/master/model.py
+@frozen
 class GPT2Initialization(InitializationStrategy):
+    has_encoder: bool = True
 
     def initialize(self, model: torch.nn.Module) -> torch.nn.Module:
-        if not hasattr(model, "encoder"):
-            raise ValueError("GPT2 Initialization requires model to have an encoder!")
-        n_layers = getattr(model.encoder, "num_layers")
-
         def init_weights(module: torch.nn.Module):
             if isinstance(module, torch.nn.Linear):
                 torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
@@ -37,7 +40,18 @@ class GPT2Initialization(InitializationStrategy):
 
         model.apply(init_weights)
 
-        for pn, p in model.named_parameters():
-            if pn.endswith("c_proj.weight"):
-                torch.nn.init.normal_(p, mean=0.0, std=0.02 / math.sqrt(2 * n_layers))
+        if self.has_encoder:
+            if not hasattr(model, "encoder"):
+                raise ValueError(
+                    "GPT2 Initialization requires model to have an encoder!"
+                )
+
+            n_layers = getattr(model.encoder, "num_layers")
+
+            for pn, p in model.named_parameters():
+                if pn.endswith("c_proj.weight"):
+                    torch.nn.init.normal_(
+                        p, mean=0.0, std=0.02 / math.sqrt(2 * n_layers)
+                    )
+
         return model
