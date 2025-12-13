869244932e18b50312ccf311e4518f45c8ae4b95
alansmacbook <milligana55@gmail.com>
2025-12-13 14:42:55 -0500

    added in memory cifar10, probably want to generalize to cifarX like resnts in future

--- optexp/datasets/__init__.py
+++ optexp/datasets/__init__.py
@@ -1,4 +1,5 @@
 from optexp.datasets.dataset import Dataset
 from optexp.datasets.dummy import DummyRegression
 from optexp.datasets.image.mnist import MNIST
+from optexp.datasets.image.cifar10 import CIFAR10
 from optexp.datasets.text.wikitext import WikiText2, WikiText103
